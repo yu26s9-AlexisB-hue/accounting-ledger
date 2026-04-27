81224d1799768b9a7c973be89d3a0fe3c46aaf63
Alexis Barrett <happynessb09@gmail.com>
2026-04-27 10:01:16 -0400

    Setting up my Main class and my Details class

---
+++ src/main/java/com/pluralsight/AccountingLedger.java
@@ -0,0 +1,7 @@
+package com.pluralsight;
+
+public class AccountingLedger {
+    public static void main(String[] args){
+
+    }
+}
---
+++ src/main/java/com/pluralsight/Details.java
@@ -0,0 +1,57 @@
+package com.pluralsight;
+
+public class Details {
+    private String date;
+    private String time;
+    private String description;
+    private String vendor;
+    private double amount;
+
+    public Details(String date, String time, String description, String vendor, double amount) {
+        this.date = date;
+        this.time = time;
+        this.description = description;
+        this.vendor = vendor;
+        this.amount = amount;
+    }
+
+    public String getDate() {
+        return date;
+    }
+
+    public void setDate(String date) {
+        this.date = date;
+    }
+
+    public String getTime() {
+        return time;
+    }
+
+    public void setTime(String time) {
+        this.time = time;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getVendor() {
+        return vendor;
+    }
+
+    public void setVendor(String vendor) {
+        this.vendor = vendor;
+    }
+
+    public double getAmount() {
+        return amount;
+    }
+
+    public void setAmount(double amount) {
+        this.amount = amount;
+    }
+}
